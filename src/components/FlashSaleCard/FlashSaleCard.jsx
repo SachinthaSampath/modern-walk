081a1ab0e78417b36add6d6a8332c759b6b6f4da
Sachintha Sampath <sachinsampath42@gmail.com>
2023-05-24 08:19:16 +0530

    main work done

--- src/components/FlashSaleCard/FlashSaleCard.jsx
+++ src/components/FlashSaleCard/FlashSaleCard.jsx
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
-import './FlashSaleCard.css'
+import "./FlashSaleCard.css";
 
 export default class FlashSaleCard extends Component {
   render() {
-    const { title, img, price, desc } = this.props.itemData;
+    const { title, price, image, id, description, category } =
+      this.props.itemData;
 
     return (
       <div className="flash-sale-card-container">
         <h2 className="flash-sale-card-title">{title}</h2>
-        <img src={img} alt="Flash sale item" className="flash-sale-card-img"/>
-        <div className="flash-sale-card-details-container">
+        <img
+          src={image}
+          alt="Flash sale item"
+          className="flash-sale-card-img"
+        />
+        <div
+          className={`flash-sale-card-details-container ${
+            category === "men's clothing" ? "card-mens" : "card-womens"
+          }`}
+        >
           <h2 className="flash-sale-card-price">Rs {price}</h2>
-          <p className="flash-sale-card-desc">{desc}</p>
+          <p className="flash-sale-card-desc">{description}</p>
         </div>
       </div>
     );
--- src/pages/WomensClothing/WomensClothing.jsx
+++ src/pages/WomensClothing/WomensClothing.jsx
@@ -6,33 +6,29 @@ import ItemCard from "../../components/ItemCard/ItemCard";
 export default class WomensClothing extends Component {
   constructor(props) {
     super(props);
-    this.flashSaleItems = [
-      {
-        id: 1,
-        title: "Mens Cotton Jacket",
-        img: "",
-        price: "55.99",
-        desc: "Great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking...",
-      },
-      {
-        id: 2,
-        title: "Women's Short Sleevs",
-        img: "",
-        price: "7.95",
-        desc: "100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk...",
-      },
-    ];
+   
+    this.state = {
+      items: [],
+    };
   }
+  
+  componentDidMount = () => {
+    fetch("https://fakestoreapi.com/products/category/women's clothing")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({items:json});
+      });
+  };
 
   render() {
-    const items = this.flashSaleItems;
+    
     return (
-      <div className="mens-clothing-main-container">
+      <div className="womens-clothing-main-container">
         <Header />
-        <div className="mens-clothing-sub-container">
+        <div className="womens-clothing-sub-container">
           <h2>Women's Clothing</h2>
-          <div className="mens-clothing-item-container">
-            {items.map((i) => {
+          <div className="womens-clothing-item-container">
+            {this.state.items.map((i) => {
               return <ItemCard itemData={i} />;
             })}
           </div>
