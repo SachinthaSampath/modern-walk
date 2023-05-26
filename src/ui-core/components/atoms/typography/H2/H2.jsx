c631d4b61d00e2969f4f4f93b356d59ab24c0d9d
Sachintha Sampath <sachinsampath42@gmail.com>
2023-05-26 15:11:05 +0530

    refactor(components): Refactor  typography components

--- src/ui-core/components/atoms/typography/H2/H2.jsx
+++ src/ui-core/components/atoms/typography/H2/H2.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "../Typography.css";
-export default function H1({children}) {
+
+export default function H1({ children }) {
   return (
-    <h1 className="main-heading">
+    <h2 className="main-heading">
       {children}
-    </h1>
+    </h2>
   );
 }
