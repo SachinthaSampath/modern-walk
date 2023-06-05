405b260f8e8467b4018aa466face230d1110b86f
Sachintha Sampath <sachinsampath42@gmail.com>
2023-06-05 14:10:47 +0530

    feat: Add login page with login form

--- src/ui-core/pages/LoginPage/LoginPage.tsx
+++ src/ui-core/pages/LoginPage/LoginPage.tsx
@@ -1,27 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./LoginPage.css";
 
 const LoginPage = () => {
+  const [uname, setUname] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitForm = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!uname.trim()) {
+      alert("Please enter valid username!");
+      return;
+    }
+    if (!password) {
+      alert("Please enter valid password!");
+      return;
+    }
+    console.log(uname, password);
+  };
+
   return (
     <div className="login-container">
       <div className="login">
-        <form id="form-login" >
-          <label>
-            <b>User Name</b>
-          </label>
-          <input type="text" name="uname" id="uname" placeholder="Username" />
-          <br />
-          <br />
-          <label>
-            <b>Password</b>
-          </label>
-          <input type="password" name="psw" id="psw" placeholder="Password" />
-          <br />
-          <br />
-          <input type="button" name="login" id="login" value="Login" />
-          <br />
-          <br /> 
+        <form id="form-login" onSubmit={submitForm}>
+          <p>
+            <label>
+              <b>User Name</b>
+            </label>
+            <input
+              value={uname}
+              onChange={(e) => setUname(e.target.value)}
+              type="text"
+              name="uname"
+              id="uname"
+              placeholder="Username"
+            />
+          </p>
+
+          <p>
+            <label>
+              <b>Password</b>
+            </label>
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              name="psw"
+              id="psw"
+              placeholder="Password"
+            />
+          </p>
+
+          <p>
+            <input type="submit" name="login" id="login" value="Login" />
+          </p>
         </form>
       </div>
     </div>
