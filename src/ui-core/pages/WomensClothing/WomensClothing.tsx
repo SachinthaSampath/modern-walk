14c082738f977efdb9e6fe04f800907407d7934e
Sachintha Sampath <sachinsampath42@gmail.com>
2023-06-08 16:12:09 +0530

    refactor: Add axios and types to Women's clothing

--- src/ui-core/pages/WomensClothing/WomensClothing.tsx
+++ src/ui-core/pages/WomensClothing/WomensClothing.tsx
@@ -1,29 +1,24 @@
-import React, { useEffect, useState } from "react"; 
+import React, { useEffect, useState } from "react";
 import Header from "../../components/molecules/Header/Header";
 import ItemCard from "../../components/molecules/ItemCard/ItemCard";
 import SectionLayout from "../../layouts/SectionLayout/SectionLayout";
 
-type Item = {
-  id: number;
-  title: string;
-  price: string;
-  description: string;
-  category: string;
-  image: string;
-  rating: {
-    rate: number;
-    count: number;
-  };
-}; 
+import { Item } from "../../../types/Item";
+import axios from "axios";
 
 export default function WomensClothing() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState<Item[] | undefined>(undefined);
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products/category/women's clothing")
-      .then((res) => res.json())
-      .then((json) => {
-        setItems(json);
+    axios({
+      method: "GET",
+      url: "https://fakestoreapi.com/products/category/women's clothing",
+    })
+      .then((res) => {
+        setItems(res.data);
+      })
+      .catch((er) => {
+        console.log(er);
       });
   }, []);
 
@@ -31,7 +26,7 @@ export default function WomensClothing() {
     <>
       <Header headingText="Modern Walk" />
       <SectionLayout heading={"Women's Clothing"}>
-        {items.map((i:Item) => {
+        {items?.map((i: Item) => {
           return <ItemCard key={i.id} itemData={i} />;
         })}
       </SectionLayout>
